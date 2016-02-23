b0386c4e339687ebd82a17d6ba4b0b2873545c60
LeoHongyi <hongyi@hongyis-MacBook-Pro.local>
2016-02-23 01:09:43 -0500

    Initial Commit

---
+++ SmartBus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartBus
+//
+//  Created by hongyi liu on 16/2/23.
+//  Copyright (c) 2016年 hongyi liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
