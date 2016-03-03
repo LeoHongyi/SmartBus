c01f7f54ead89514a69c6a9864583b9fb4a62b17
LeoHongyi <hongyi@vpn28-187.njit.edu>
2016-03-03 17:11:57 -0500

    hold_request complete!

--- LHYQueryViewController.m
+++ LHYQueryViewController.m
@@ -10,12 +10,14 @@
 #import "MBProgressHUD+HM.h"
 @interface LHYQueryViewController ()
 @property(nonatomic,strong)NSString *fid;
+@property(nonatomic,strong)NSString *p_hold_requset;
 @property (weak, nonatomic) IBOutlet UILabel *p_next_delay;
 @property (weak, nonatomic) IBOutlet UILabel *poBusArrT;
 
 @property (weak, nonatomic) IBOutlet UILabel *podBusArrT;
-
 @property (weak, nonatomic) IBOutlet UIButton *request;
+
+
 @end
 
 @implementation LHYQueryViewController
@@ -44,8 +46,40 @@
             
                             }];
         self.request.enabled = YES;
-        self.request.backgroundColor = [UIColor redColor];
+//        self.request.backgroundColor = [UIColor redColor];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"REQUEST" message:@"Do you want to hold request" preferredStyle:UIAlertControllerStyleActionSheet];
+        [alert addAction:[UIAlertAction actionWithTitle:@"cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+            
+        }]];
+        [alert addAction:[UIAlertAction actionWithTitle:@"REQUEST" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            [MBProgressHUD showMessage:@"update..."];
+            NSURL *url1 = [NSURL URLWithString:@"http://localhost:8080/SmartBus/TestHold"];
+            NSMutableURLRequest *request1 = [NSMutableURLRequest requestWithURL:url1];
+            request1.timeoutInterval = 5;
+            request1.HTTPMethod = @"post";
+            NSLog(@"%@",self.fid);
+            self.p_hold_requset = @"1";
+            NSString *param1 = [NSString stringWithFormat:@"p_hold_requset=%d&fid=%@",[self.p_hold_requset intValue],self.fid];
+            request1.HTTPBody = [param1 dataUsingEncoding:NSUTF8StringEncoding];
+            NSOperationQueue *queue = [NSOperationQueue currentQueue];
+            [NSURLConnection sendAsynchronousRequest:request1 queue:queue completionHandler:^(NSURLResponse *response, NSData *data1, NSError *connectionError) {
+                [MBProgressHUD hideHUD];
+                NSString *str1 = [[NSString alloc]initWithData:data1 encoding:NSUTF8StringEncoding];
+                NSLog(@"%d",[str1 intValue]);
+                if ([str1 intValue] == 1) {
+                    [MBProgressHUD showSuccess:@"update success"];
+                }else{
+                    [MBProgressHUD showError:@"error"];
+                }
+                
+            }];
+            
+            
+        }]];
         
+        
+        [self presentViewController:alert animated:YES completion:nil];
+      
         
     });
                         
@@ -54,10 +88,43 @@
 
 }
 
-- (IBAction)holdRequest:(id)sender {
-    
+- (IBAction)request:(id)sender {
+//    
+//    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"REQUEST" message:@"Do you want to hold request" preferredStyle:UIAlertControllerStyleActionSheet];
+//    [alert addAction:[UIAlertAction actionWithTitle:@"cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+//        
+//    }]];
+//    [alert addAction:[UIAlertAction actionWithTitle:@"REQUEST" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+//        [MBProgressHUD showMessage:@"update..."];
+//        NSURL *url1 = [NSURL URLWithString:@"http://localhost:8080/SmartBus/TestHold"];
+//        NSMutableURLRequest *request1 = [NSMutableURLRequest requestWithURL:url1];
+//        request1.timeoutInterval = 5;
+//        request1.HTTPMethod = @"post";
+//        NSLog(@"%@",self.fid);
+//        self.p_hold_requset = @"1";
+//        NSString *param1 = [NSString stringWithFormat:@"p_hold_requset=%d&fid=%@",[self.p_hold_requset intValue],self.fid];
+//        request1.HTTPBody = [param1 dataUsingEncoding:NSUTF8StringEncoding];
+//        NSOperationQueue *queue = [NSOperationQueue currentQueue];
+//        [NSURLConnection sendAsynchronousRequest:request1 queue:queue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+//            [MBProgressHUD hideHUD];
+//            NSString *str = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+//            NSLog(@"%d",[str intValue]);
+//            if ([str intValue] == 1) {
+//                [MBProgressHUD showSuccess:@"update success"];
+//            }else{
+//                [MBProgressHUD showError:@"error"];
+//            }
+//            
+//        }];
+//        
+//        
+//    }]];
+//    
+//    
+//    [self presentViewController:alert animated:YES completion:nil];
     
 }
+
 
 
 
