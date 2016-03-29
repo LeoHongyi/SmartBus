8ce501ea3a298b88f407881be71267a5cd79ac25
LeoHongyi <hongyi@hongyis-MacBook-Pro.local>
2016-03-28 22:34:31 -0400

    frequently send request and receive response server remotely

--- LHYQueryViewController.m
+++ LHYQueryViewController.m
@@ -19,7 +19,10 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *hold_success;
 @property (assign,nonatomic) int mainInt;
-@property NSTimer *timer;
+@property (nonatomic, strong) dispatch_source_t timer;
+@property (weak, nonatomic) IBOutlet UILabel *warningField;
+
+
 
 @end
 
@@ -29,101 +32,26 @@
     [super viewDidLoad];
     
     
-    self.timer = [NSTimer timerWithTimeInterval:5.0 target:self selector:@selector(sendRequestToGetData:) userInfo:nil repeats:YES];
-    self.mainInt = 5;
-    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSDefaultRunLoopMode];
+    //self.timer = [NSTimer timerWithTimeInterval:3.0 target:self selector:@selector(sendRequestToGetData:) userInfo:nil repeats:YES];
+    //self.mainInt = 5;
+    //[[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSDefaultRunLoopMode];
     
-    
-    // Do any additional setup after loading the view.
-//    [MBProgressHUD showMessage:@"query..."];
-//    self.fid = [[[UIDevice currentDevice]identifierForVendor] UUIDString];
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1*NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        [MBProgressHUD hideHUD];
-//         NSURL *url = [NSURL URLWithString:@"http://localhost:8080/SmartBus/TestQuery"];
-//        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-//        request.timeoutInterval = 5;
-//                        request.HTTPMethod = @"post";
-//                        NSString *param1 = [NSString stringWithFormat:@"fid=%@",self.fid];
-//                        request.HTTPBody = [param1 dataUsingEncoding:NSUTF8StringEncoding];
-//                        NSOperationQueue *queue = [NSOperationQueue currentQueue];
-//        [NSURLConnection sendAsynchronousRequest:request queue:queue completionHandler:^(NSURLResponse *response, NSData *data1, NSError *connectionError) {
-//                                NSArray *arr = [NSJSONSerialization JSONObjectWithData:data1 options:kNilOptions error:nil];
-//            NSDictionary *dict = arr[arr.count-1];
-//            NSLog(@"%@",dict);
-//            self.p_next_delay.text = dict[@"p_next_delay"];
-//            self.poBusArrT.text = dict[@"poBusArrT"];
-//            self.podBusArrT.text = dict[@"podBusArrT"];
-//            
-//                            }];
-//        self.request.enabled = YES;
-////        self.request.backgroundColor = [UIColor redColor];
-//        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"REQUEST" message:@"Do you want to hold request" preferredStyle:UIAlertControllerStyleActionSheet];
-//        [alert addAction:[UIAlertAction actionWithTitle:@"cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
-//            
-//        }]];
-//        [alert addAction:[UIAlertAction actionWithTitle:@"REQUEST" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-//            [MBProgressHUD showMessage:@"update..."];
-//            NSURL *url1 = [NSURL URLWithString:@"http://localhost:8080/SmartBus/TestHold"];
-//            NSMutableURLRequest *request1 = [NSMutableURLRequest requestWithURL:url1];
-//            request1.timeoutInterval = 5;
-//            request1.HTTPMethod = @"post";
-//            NSLog(@"%@",self.fid);
-//            self.p_hold_requset = @"1";
-//            NSString *param1 = [NSString stringWithFormat:@"p_hold_requset=%d&fid=%@",[self.p_hold_requset intValue],self.fid];
-//            request1.HTTPBody = [param1 dataUsingEncoding:NSUTF8StringEncoding];
-//            NSOperationQueue *queue = [NSOperationQueue mainQueue];
-//            [NSURLConnection sendAsynchronousRequest:request1 queue:queue completionHandler:^(NSURLResponse *response, NSData *data1, NSError *connectionError) {
-//                [MBProgressHUD hideHUD];
-//                NSString *str1 = [[NSString alloc]initWithData:data1 encoding:NSUTF8StringEncoding];
-//                NSLog(@"%d",[str1 intValue]);
-//                if ([str1 intValue] == 1) {
-//                    [MBProgressHUD showSuccess:@"update success"];
-//                    
-//                    [MBProgressHUD showMessage:@"Holding Query...."];
-//                    NSURL *url2 = [NSURL URLWithString:@"http://localhost:8080/SmartBus/TestHoldQuery"];
-//                    NSMutableURLRequest *request2 = [NSMutableURLRequest requestWithURL:url2];
-//                    request2.timeoutInterval = 5;
-//                    request2.HTTPMethod = @"post";
-//                    NSString *param2 = [NSString stringWithFormat:@"fid=%@",self.fid];
-//                    request2.HTTPBody = [param2 dataUsingEncoding:NSUTF8StringEncoding];
-//                    NSOperationQueue *queue = [NSOperationQueue mainQueue];
-//                    [NSURLConnection sendAsynchronousRequest:request2 queue:queue completionHandler:^(NSURLResponse *response, NSData *data2, NSError *connectionError) {
-//                        [MBProgressHUD hideHUD];
-//                        NSArray *arr = [NSJSONSerialization JSONObjectWithData:data2 options:kNilOptions error:nil];
-//                        NSLog(@"%@",arr);
-//                        NSDictionary *dict = arr[arr.count-1];
-//                        self.hold_success.text = dict[@"p_hold_success"];
-//                        
-//                    }];
-//                    
-//                    
-//                    
-//                }else{
-//                    [MBProgressHUD showError:@"error"];
-//                }
-//                
-//            }];
-//            
-//            
-//        }]];
-//        
-//        
-//        [self presentViewController:alert animated:YES completion:nil];
-//      
-//        
-//    });
-    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self sendRequestToGetData];
+        
+    });
 
     
 
 }
 
 
--(void)sendRequestToGetData:(NSTimer *)timer
+-(void)sendRequestToGetData
 {
-    self.mainInt -= 1;
+    //self.mainInt -= 1;
     [MBProgressHUD showMessage:@"query..."];
-    self.fid = [[[UIDevice currentDevice]identifierForVendor] UUIDString];
+    //self.fid = [[[UIDevice currentDevice]identifierForVendor] UUIDString];
+    self.fid = @"238a11fd7ca11bd";
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1*NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [MBProgressHUD hideHUD];
         NSURL *url = [NSURL URLWithString:@"http://localhost:8080/SmartBus/TestQuery"];
@@ -135,11 +63,24 @@
         NSOperationQueue *queue = [NSOperationQueue currentQueue];
         [NSURLConnection sendAsynchronousRequest:request queue:queue completionHandler:^(NSURLResponse *response, NSData *data1, NSError *connectionError) {
             NSArray *arr = [NSJSONSerialization JSONObjectWithData:data1 options:kNilOptions error:nil];
-            NSDictionary *dict = arr[arr.count-1];
-            NSLog(@"%@",dict);
+           // NSLog(@"%lu",(unsigned long)arr.count);
+//            NSLog(@"%lu",(unsigned long)arr.count);
+            NSLog(@"%@",arr[0]);
+            NSDictionary *dict = arr[0];
+            NSLog(@"%@",dict[@"warn"]);
+            if (dict[@"warn"] == NULL) {
+                 self.poBusArrT.text = dict[@"Connecting Bus Schedule Arrival Time at Transfer Stop"];
+                 self.podBusArrT.text = dict[@"First Bus Schedule Arrival Time at Transfer Stop"];
+            }else{
+                self.warningField.text = dict[@"warn"];
+            }
             
-            self.poBusArrT.text = dict[@"ptoBusArrT"];
-           self.podBusArrT.text = dict[@"podBusArrT"];
+                        //if (dict[@"Connecting Bus Schedule Arrival Time at Transfer Stop"] == NULL) {
+               // self.warningField.text = arr[0];
+            //}
+           // self.poBusArrT.text = dict[@"Connecting Bus Schedule Arrival Time at Transfer Stop"];
+           //self.podBusArrT.text = dict[@"First Bus Schedule Arrival Time at Transfer Stop"];
+          
            // NSLog(@"%@",dict[@"podBusArrT"]);
             
         }];
@@ -157,6 +98,8 @@
             request1.HTTPMethod = @"post";
             NSLog(@"%@",self.fid);
             self.p_hold_requset = @"1";
+            //
+            
             NSString *param1 = [NSString stringWithFormat:@"p_hold_requset=%d&fid=%@",[self.p_hold_requset intValue],self.fid];
             request1.HTTPBody = [param1 dataUsingEncoding:NSUTF8StringEncoding];
             NSOperationQueue *queue = [NSOperationQueue mainQueue];
@@ -180,10 +123,63 @@
                         NSArray *arr = [NSJSONSerialization JSONObjectWithData:data2 options:kNilOptions error:nil];
                         NSLog(@"%@",arr);
                         NSDictionary *dict = arr[arr.count-1];
+                        NSLog(@"%@",dict);
                         self.hold_success.text = dict[@"p_hold_success"];
                         
                     }];
-                    
+                   
+                    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+                    self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, mainQueue);
+                    dispatch_time_t start = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC));
+                    uint64_t interval = (uint64_t)(20.0 * NSEC_PER_SEC);
+                    dispatch_source_set_timer(self.timer, start, interval, 0);
+                    dispatch_source_set_event_handler(self.timer, ^{
+                        
+                        NSLog(@"----");
+                        NSURL *url3 = [NSURL URLWithString:@"http://localhost:8080/SmartBus/TestRoute"];
+                        NSMutableURLRequest *request3 = [NSMutableURLRequest requestWithURL:url3];
+                        request3.timeoutInterval = 5;
+                        request3.HTTPMethod = @"post";
+                        NSString *param2 = [NSString stringWithFormat:@"fid=%@",self.fid];
+                        request3.HTTPBody = [param2 dataUsingEncoding:NSUTF8StringEncoding];
+                        NSOperationQueue *queue = [NSOperationQueue mainQueue];
+                        [NSURLConnection sendAsynchronousRequest:request3 queue:queue completionHandler:^(NSURLResponse *response, NSData *data3, NSError *connectionError) {
+                            
+                            NSString *str3 = [[NSString alloc]initWithData:data3 encoding:NSUTF8StringEncoding];
+                            if ([str3  isEqual: @"arrive"]) {
+                                
+                                dispatch_cancel(self.timer);
+                                self.timer = nil;
+                                
+                                [MBProgressHUD showMessage:str3];
+                                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4*NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                                    [MBProgressHUD hideHUD];
+                                });
+                            } else {
+                               [MBProgressHUD showMessage:str3];
+                                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4*NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                                    [MBProgressHUD hideHUD];
+                                });
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                            }
+                            
+                        }];
+                       
+                    });
+                    dispatch_resume(self.timer);
                     
                     
                 }else{
@@ -196,52 +192,22 @@
         }]];
         
         
+        
+        
         [self presentViewController:alert animated:YES completion:nil];
         
         
     });
-    if(self.mainInt == 0){
-        [self.timer invalidate];
-    }
+//    if(self.mainInt == 0){
+//        [self.timer invalidate];
+//    }
     
     
 
 }
 
 - (IBAction)request:(id)sender {
-//
-//    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"REQUEST" message:@"Do you want to hold request" preferredStyle:UIAlertControllerStyleActionSheet];
-//    [alert addAction:[UIAlertAction actionWithTitle:@"cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
-//        
-//    }]];
-//    [alert addAction:[UIAlertAction actionWithTitle:@"REQUEST" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-//        [MBProgressHUD showMessage:@"update..."];
-//        NSURL *url1 = [NSURL URLWithString:@"http://localhost:8080/SmartBus/TestHold"];
-//        NSMutableURLRequest *request1 = [NSMutableURLRequest requestWithURL:url1];
-//        request1.timeoutInterval = 5;
-//        request1.HTTPMethod = @"post";
-//        NSLog(@"%@",self.fid);
-//        self.p_hold_requset = @"1";
-//        NSString *param1 = [NSString stringWithFormat:@"p_hold_requset=%d&fid=%@",[self.p_hold_requset intValue],self.fid];
-//        request1.HTTPBody = [param1 dataUsingEncoding:NSUTF8StringEncoding];
-//        NSOperationQueue *queue = [NSOperationQueue currentQueue];
-//        [NSURLConnection sendAsynchronousRequest:request1 queue:queue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-//            [MBProgressHUD hideHUD];
-//            NSString *str = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
-//            NSLog(@"%d",[str intValue]);
-//            if ([str intValue] == 1) {
-//                [MBProgressHUD showSuccess:@"update success"];
-//            }else{
-//                [MBProgressHUD showError:@"error"];
-//            }
-//            
-//        }];
-//        
-//        
-//    }]];
-//    
-//    
-//    [self presentViewController:alert animated:YES completion:nil];
+
     
 }
 
